461deec64ec944f3010a4dcb2ec0c7cc8b649e60
Abhinav Utkarsh <abhinav.utkarsh@gmail.com>
2020-06-01 21:12:34 +0530

    webpage styled with css & api integrated

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,21 @@
 const path = require("path")
 const express = require("express")
+const hbs = require("hbs")
+const forecast = require(path.join(__dirname, "/utils/forecast"))
+const geocode = require(path.join(__dirname, "/utils/geocode"))
 
 const app = express()
 
 // Define paths for Express config
 const publicDir = path.join(__dirname, "../public")
-const viewsPath = path.join(__dirname, "../templates")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs")
 app.set("views", viewsPath)
+hbs.registerPartials(partialsPath)
 
 // setup static directory to serve
 app.use(express.static(publicDir))
@@ -30,10 +36,50 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
     res.render("help", {
-        helpText: "May the force be with you"
+        title: "Help",
+        name: "Dev Kitty"
     })
 })
 
-app.listen(3005, () => {
-    console.log("Server is up and running on port 3005")
+app.get("/weather", (req, res) => {
+    if (!req.query.address) {
+        return res.send({ error: "provide an address" })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+
+})
+
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: "Dev Kitty",
+        error: "help article not found"
+    })
+})
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: "Dev Kitty",
+        error: "page not found"
+    })
+})
+
+const port = 3010
+app.listen(port, () => {
+    console.log("Server is up and running on port 3010")
 })
